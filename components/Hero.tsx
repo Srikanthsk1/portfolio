4e2188c3e16cd83e2f3523fb5e988b12b6cede52
Srikanthsk1 <srikanth6374402154@gmail.com>
2026-01-15 17:48:57 +0530

    image changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -78,7 +78,7 @@ const Hero: React.FC = () => {
                   alt="Alex Portrait" 
                   className="w-full h-full object-cover brightness-[1.02] group-hover:scale-105 transition-all duration-1000 grayscale-[20%] group-hover:grayscale-0"
                   onError={(e) => {
-                    (e.target as HTMLImageElement).src = "src/assets/bannerImg.png";
+                    (e.target as HTMLImageElement).src = "https://drive.google.com/file/d/1Bkwx_FDPcW6sAB_bAg4KvVL2UDzBv0WZ/view?usp=sharing";
                   }}
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-slate-950 via-slate-950/10 to-transparent" />
